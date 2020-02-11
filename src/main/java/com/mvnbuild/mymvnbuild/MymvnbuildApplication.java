0edca7bc1ea61a464faff0f5c2e943ad3bbe29ad
gabehaegele <Haegeleg@gmail.com>
2020-02-11 13:02:35 -0600

    Initial Commit

---
+++ src/main/java/com/mvnbuild/mymvnbuild/MymvnbuildApplication.java
@@ -0,0 +1,28 @@
+package com.mvnbuild.mymvnbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class MymvnbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymvnbuildApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingController {
+
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name) {
+		return "Hello, " + name + "!";
+	}
+}
---
+++ src/test/java/com/mvnbuild/mymvnbuild/MymvnbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvnbuild.mymvnbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymvnbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
